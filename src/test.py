6691a43abf6400484ce794737f2323c4ea56d22f
Manju Shettar <manjshettar@gmail.com>
2023-11-03 15:34:21 -0700

    Hi

--- src/test.py
+++ src/test.py
@@ -3,6 +3,8 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, ClockCycles
 from cocotb.binary import BinaryValue
 
+THRESHOLD = 0x8000 
+
 async def reset(dut):
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 5)
@@ -28,13 +30,14 @@ async def test_no_spike(dut):
 
 @cocotb.test()
 async def test_spike(dut):
-    await reset(dut)
-
-    spike_value = BinaryValue("11111111")
-    dut.ui_in = spike_value
-    dut.uio_in = spike_value
-
-    await ClockCycles(dut.clk, 100)
-
-    spikes = dut.uo_out.value.binstr[-2:]
-    assert spikes == "11", "Spikes were not generated as expected"
+    dut.rst_n.value = 0
+    await RisingEdge(dut.clk)
+    dut.rst_n.value = 1
+    await RisingEdge(dut.clk)
+    
+    dut.spike_current.value = THRESHOLD + 1
+    
+    await RisingEdge(dut.clk)
+    
+    assert dut.spike.value == 1, "Expected a spike when current exceeds the threshold"
+    
